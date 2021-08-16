0f27887e7bf9c4a7ea503294688909c553dd08b3
Shirly <shirlyd@axioned.com>
2021-08-16 16:26:56 +0530

    js initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-/* Author: 
+var hide = document.querySelector('.hide');
+var text = document.querySelector('.more');
+var btn = document. querySelector('.box-btn');
 
-*/
-
+btn.addEventListener('click', function(e){
+    
+})
 
 
 
