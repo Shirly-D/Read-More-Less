38b8d7c20c85db31a3a36f3a268f250ace9f9cf5
Shirly <shirlyd@axioned.com>
2021-08-16 17:33:40 +0530

    js functions commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
-var hide = document.querySelector('.hide');
-var text = document.querySelector('.more');
-var btn = document. querySelector('.box-btn');
+var hide = document.querySelectorAll('.hide');
+var text = document.querySelectorAll('.more');
+var btn = document. querySelectorAll('.box-btn');
+var para = document.querySelectorAll('p');
+console.log(para);
 
-btn.addEventListener('click', function(e){
+var words = 0;
+
+function read() {
+    for (var i = 0; i < para.length; i++) {
+        btn[i].addEventListener('click', function(e){
+            if (!words) {
+                text.classList.add("active");
+                hide.classList.add('more');
+                btn.innerHTML = "Read Less";
+                words = 1;
+                console.log(words);
+            } else {
+                hide.classList.add("active");
+                btn.innerHTML = "Read More";
+                text.classList.add('more');
+                words = 0;
+            }
     
-})
+        })
+            }
+    
+    
+}
 
 
 
