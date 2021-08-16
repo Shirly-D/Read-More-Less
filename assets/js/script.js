12f7c1c05077e5385391d15dc3d067250c47b544
Shirly <shirlyd@axioned.com>
2021-08-16 19:54:25 +0530

    js final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,12 @@ for (var i = 0; i < list.length; i++) {
     list[i].addEventListener('click', function(e){
         var current = e.target;
         var readMore = current.className.includes('box-btn');
-        console.log(current);
-        console.log(readMore);
         if (!readMore){ 
             return;
         }
-        console.log(list[i]);
-        console.log(list.length);
         var currentText = e.target.parentNode.parentNode.querySelector('.more');
-        console.log(currentText);
         currentText.classList.toggle('active');
         current.textContent = current.textContent.includes('read more') ? 'read less' : 'read more';
-        // currentText.classList.remove('more');
-        console.log(current.textContent);
     })
     
 }
